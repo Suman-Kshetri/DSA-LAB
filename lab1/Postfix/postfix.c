b9909a1eab71961053efc66d381c35bf96aa5625
Suman-Kshetri <ksuman20601028@gmail.com>
2025-06-13 09:20:07 +0545

    refactor(postfix): reorganize code structure and improve readability

--- lab1/Postfix/postfix.c
+++ lab1/Postfix/postfix.c
@@ -1,59 +1,80 @@
-#include<stdio.h>
-#include<math.h>
-#include<ctype.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
 #include <stdlib.h>
-#define MAX_SIZE 100
+#include <ctype.h>
 
-
-typedef struct stack {
-    int top;
-    float data[MAX_SIZE];
-} Stack;
-
-float push(Stack *stack, int value) {
-    // stack->top += 1;
-    return stack->data[++stack->top] = value;
+#include <math.h>
+#define MAX 100
+struct stack
+{
+    int TOS;
+    float data[MAX];
+};
+void push(struct stack *s, float element)
+{
+    if (s->TOS >= MAX - 1)
+    {
+        printf("Stack overflow!\n");
+        exit(1);
+    }
+    s->TOS += 1;
+    s->data[s->TOS] = element;
 }
-
-float pop(Stack *stack) {
-    return stack->data[stack->top--];
+float pop(struct stack *s)
+{
+    if (s->TOS < 0)
+    {
+        printf("Stack underflow!\n");
+        exit(1);
+    }
+    return s->data[s->TOS--];
 }
-
-int main() {
-    Stack s = {-1};
-    float op1, op2, result = 0;
+int main()
+{
+    float op1, op2;
+    float result;
+    int choice;
+    struct stack s = {-1};
     char exp[100];
-
-    printf("Enter the postfix expression: ");
-    fgets(exp, sizeof(exp), stdin);
-
-    char *token = strtok(exp, " \n");
-    while (token != NULL) {
-        if (isdigit(token[0]) || (token[0] == '-' && isdigit(token[1]))) {
+    printf("Enter the postfix expression: \n");
+    fgets(exp, 100, stdin);
+    char *token = strtok(exp, " ");
+    while (token != NULL)
+    {
+        if (isdigit(token[0]) || (token[0] == '-' && isdigit(token[1])))
+        {
             push(&s, atof(token));
-        } else {
+        }
+        else
+        {
             op1 = pop(&s);
             op2 = pop(&s);
-            switch (token[0]) {
-                case '^': result = pow(op2, op1); break;
-                case '*': result = op2 * op1; break;
-                case '/': result = op2 / op1; break;
-                case '-': result = op2 - op1; break;
-                case '+': result = op2 + op1; break;
-                default:
-                    printf("Unknown operator: %s\n", token);
-                    return 1;
+            switch (token[0])
+            {
+            case '^':
+                result = pow(op2, op1);
+                break;
+            case '*':
+                result = op2 * op1;
+                break;
+            case '/':
+                result = op2 / op1;
+                break;
+            case '-':
+                result = op2 - op1;
+                break;
+            case '+':
+                result = op2 + op1;
+                break;
             }
             push(&s, result);
         }
-        token = strtok(NULL, " \n");
+        token = strtok(NULL, " ");
     }
-
-    if (s.top == 0)
-        printf("The result of the given expression is: %.2f\n", pop(&s));
+    if (s.TOS == 0)
+        printf("The result of the given postfix expression is %f \n", result);
     else
-        printf("Invalid postfix expression.\n");
-
+        printf("Invalid postfix expression.");
     return 0;
 }
